70d09559d76c97f2859c435e5d65f1395fcaea6f
Everett <everett.bowline@gmail.com>
2018-06-14 01:49:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,23 +2,23 @@ class Application
   
   @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
   
-  def call(env)
+ def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path.match(/items/)
-      stocked_item = req.path.split("/items/").last
-      if Item == @@items.find { | item | item.name = stocked_item }
-        resp.write "#{item.name}, #{item.price}"
-      else
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
         resp.status = 400
         resp.write "Item not found"
       end
     else
-      resp.status = 404
+      resp.status=404
       resp.write "Route not found"
     end
     resp.finish
-  end
+end
   
 end
