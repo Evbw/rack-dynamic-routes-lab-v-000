41afaee98eed56088513446891d66ffabdf36346
Everett <everett.bowline@gmail.com>
2018-06-14 01:42:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
       item = @@items
-      if
+      if req.path ==
       resp.write "#{item.name}, #{item.price}"
       else
         resp.status = 400
