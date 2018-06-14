c2c5a92ab1e7b95b90bac7cd536dc2903e7be410
Everett <everett.bowline@gmail.com>
2018-06-14 01:29:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path.match"items"
+    if req.path == "#{Item.name}"
       do the thing
     else
       resp.write "Route not found"
