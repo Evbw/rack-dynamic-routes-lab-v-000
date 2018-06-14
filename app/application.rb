98bcb1bf7d454fbc6e38324717e529f0b1cc2701
Everett <everett.bowline@gmail.com>
2018-06-14 01:44:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item = @@items
+      item = req.path.split("/items/").last
       if req.path == "#{@@items.name}"
         resp.write "#{@@items.name}, #{@@items.price}"
       else
